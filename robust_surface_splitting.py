c8d3bde60c3ed5bc29cdaf13777003fbc354b4a5
Abhinav  Kashyap <abhinavkashyap92@gmail.com>
2015-11-02 18:03:02 +0800

    changed the surface splitting code

--- robust_surface_splitting.py
+++ robust_surface_splitting.py
@@ -14,11 +14,16 @@ class RobustSurfaceSplitting():
         """
         self.pointcloud = np.matrix(point_cloud)
         self.lineFitting = LineFitting(np.matrix(np.genfromtxt(initial_line_points_file)))
-        self.line_params = self.lineFitting.get_lineparameters()[0]  # [theta0, theta1] shape 2,1
+        self.line_params, initial_error = self.lineFitting.get_lineparameters()  # [theta0, theta1] shape 2,1
+        print "initial error", initial_error
         self.Q = 50  # Q is the number of points close to the splitting line
         self.number_iterations = 2
-        # self.fig = plt.figure()
-        # self.axis = self.fig.add_subplot(111, projection="3d")
+        self.p1, self.p2 = self.__splitPointCloud()
+        P1 = np.array(self.p1)
+        P2 = np.array(self.p2)
+        self.param_linear_surface, self.DLinearSurface = LinSurfFit(P1[:, 0], P1[:, 1], P1[:, 2])
+        self.param_curved_surface, self.DQuadraticSurface = QuadSurfFit(P2[:, 0], P2[:, 1], P2[:, 2])
+
 
     def __splitPointCloud(self):
         """
@@ -40,32 +45,30 @@ class RobustSurfaceSplitting():
         # Step 3: Find the Distance D = | S1(x, y) - S2(x, y) |
         # Step 4: Find the subset of Points Q that have least distance
         # Step 5: Fit a line to this set of points. Repeat
-        count = self.number_iterations
-        P1, P2 = self.__splitPointCloud()  # P1 P2 contain the points that are split according to the sign
-        P1_array = np.array(P1)
-        P2_array = np.array(P2)
-        parameters_lin_surface, DLinear = LinSurfFit(P1_array[:, 0], P1_array[:, 1], P1_array[:, 2])
-        parameters_curved_surface, DQuadratic = QuadSurfFit(P2_array[:, 0], P2_array[:, 1], P2_array[:, 2])
-        P = np.vstack((P1, P2))
-        D = []
-        for eachRow in P:
-            x = eachRow.item(0, 0)
-            y = eachRow.item(0, 1)
-            S1xy = np.matrix([[x, y, 1]]) * parameters_lin_surface
-            S2xy = np.matrix([[x ** 2, y ** 2, x * y, x, y, 1]]) * parameters_curved_surface
-            D.append(np.abs(S1xy - S2xy).item(0, 0))
-        D = np.array(D)
-        minimumQPoints = np.argsort(D)[:50]
-        QPoints = P[minimumQPoints]
-        self.lineFitting = LineFitting(QPoints)
-        self.line_params, error = self.lineFitting.get_lineparameters()
-        print 'count: ', count
-        count = count - 1
-        self.number_iterations = count
-        if count != 0:
-            self.split()
+        for i in xrange(self.number_iterations-1):
 
-        return P1, P2, parameters_lin_surface, parameters_curved_surface, self.line_params
+            x = self.pointcloud[:, 0]
+            y = self.pointcloud[:, 1]
+            x2 = np.square(x)
+            y2 = np.square(y)
+            xy = np.multiply(x, y)
+            ones = np.ones((x.shape[0], 1))
+            S1xy = np.hstack((x,y, ones)) * self.param_linear_surface
+            S2xy = np.hstack((x2, y2, xy, x, y, ones)) * self.param_curved_surface
+            D = np.abs(np.subtract(S1xy, S2xy))
+            D = np.array(D).ravel()
+            minimumQPoints = np.argsort(D)[:50]
+            QPoints = self.pointcloud[minimumQPoints]
+            self.lineFitting = LineFitting(QPoints)
+            self.line_params, error = self.lineFitting.get_lineparameters()
+            print "error: ", error
+            self.p1, self.P2 = self.__splitPointCloud()
+            P1 = np.array(self.p1)
+            P2 = np.array(self.p2)
+            self.param_linear_surface, self.DLinearSurface = LinSurfFit(P1[:, 0], P1[:, 1], P1[:, 2])
+            self.param_curved_surface, self.DQuadraticSurface = QuadSurfFit(P2[:, 0], P2[:, 1], P2[:, 2])
+
+        return self.p1, self.p2, self.param_linear_surface, self.param_curved_surface, self.line_params
 
 
 if __name__ == "__main__":
